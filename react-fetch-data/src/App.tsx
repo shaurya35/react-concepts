229a507e75629bfad5fcc15e1abf8eba9d00ab79
Shaurya <shauryajha35@gmail.com>
2025-10-15 02:31:29 +0530

    Add fetch logic

--- react-fetch-data/src/App.tsx
+++ react-fetch-data/src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Item {
+  id: number;
+  userId: number;
+  title: string;
+  completed: boolean;
 }
 
-export default App
+function App() {
+  const [data, setData] = React.useState<Item[]>([]);
+
+  React.useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch(
+          "https://jsonplaceholder.typicode.com/todos" 
+        );
+        const json = await res.json();
+        setData(json);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    fetchData();
+  }, []);
+
+  if (data.length === 0) return <div>Loading...</div>;
+
+  return (
+    <div>
+      {data.map((item) => (
+        <div key={item.id}>
+          <p>User ID: {item.userId}</p>
+          <p>Title: {item.title}</p>
+          <p>Completed: {item.completed ? "Yes" : "No"}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
